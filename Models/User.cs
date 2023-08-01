295ea27d2e451712089b39e6c605f908a7791cb8
Jos Kotze <jpkotze148@gmail.com>
2023-08-01 20:07:52 +0200

    GenerateUserID

--- Models/User.cs
+++ Models/User.cs
@@ -42,9 +42,22 @@ namespace ChatApp.Models
 
         //Functions:
 
-        public void GenerateUserID()
-        {
 
+        public static string GenerateUserID()
+        {
+            Random random = new Random();
+            string[] abc = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int i = random.Next(0, 26);
+            int x = random.Next(0, 26);
+
+            // only called when registered. So where does that happen?
+
+            string part1 = abc[x] + abc[i];
+            string part2 = DateTime.Now.Ticks.ToString();
+            int part3 = random.Next(1000, 10000);
+            string uniqueID = $"{part2}{part3}";
+            string UserID = part1 + uniqueID.Substring(0, 6);
+            return UserID;
         }
 
         public void EnterUserName()
