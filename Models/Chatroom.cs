d89537b7cf65e33451d9ab8907151b70a8b65ca2
Jos Kotze <jpkotze148@gmail.com>
2023-08-01 21:43:55 +0200

    CreateRoomJoinCode function

--- Models/Chatroom.cs
+++ Models/Chatroom.cs
@@ -32,5 +32,35 @@ namespace ChatApp.Models
             // get log.txt file from server and convert to list or array.
 
         }
+
+        public void CreateRoom()
+        {
+            // get log.txt file from server and convert to list or array.
+
+        }
+
+        static string GenerateRoomJoinCode()
+        {   // only called when registered. So where does that happen?
+
+            Random random = new Random();
+            string[] abc = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string q = abc[random.Next(0, 26)];
+            string w = abc[random.Next(0, 26)];
+            string e = abc[random.Next(0, 26)];
+            string r = abc[random.Next(0, 26)];
+            string t = abc[random.Next(0, 26)];
+            string y = abc[random.Next(0, 26)];
+
+            string part1 = q + w + e; // PART 1: starts with 3 random letters
+
+            string part2 = ((DateTime.Now.Millisecond + DateTime.Now.Second) * 3).ToString(); // 3 random numbers
+
+            string part3 = r + t + y; // PART 3: ends with 3 random letters
+
+            string UserID = $"{part1}{part2}{part3}"; // string concatenation
+
+            Console.WriteLine(UserID);
+            return UserID;
+        }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -43,23 +43,28 @@ namespace ChatApp.Models
         //Functions:
 
 
-        public static string GenerateUserID()
-        {
-            Random random = new Random();
+        static string GenerateUserID()
+        {   // only called when registered. So where does that happen?
+
+            Random random = new Random(); // PART 1: starts with 2 random letters
             string[] abc = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             int i = random.Next(0, 26);
             int x = random.Next(0, 26);
-
-            // only called when registered. So where does that happen?
-
             string part1 = abc[x] + abc[i];
-            string part2 = DateTime.Now.Ticks.ToString();
-            int part3 = random.Next(1000, 10000);
-            string uniqueID = $"{part2}{part3}";
-            string UserID = part1 + uniqueID.Substring(0, 6);
+
+            string part2 = DateTime.Now.Millisecond.ToString() + DateTime.Now.Second.ToString();
+            // PART 2: Is a combination of numerical digits that are from milliseconds and then seconds
+
+            Random random2 = new Random(); // PART 3: ends with 2 random letters
+            int e = random.Next(0, 26);
+            int y = random.Next(0, 26);
+            string part3 = abc[e] + abc[y];
+
+            string UserID = $"{part1}{part2}{part3}"; // string concatenation
+
+            Console.WriteLine(UserID);
             return UserID;
         }
-
         public void EnterUserName()
         {
 
